dc12050c5e8199bab12c964bf55312509c5307e9
YasseRRRRRRRR <yassermoussaddak3@gmail.com>
2024-08-15 19:45:44 +0300

    implemented i18n and stripe skeleton

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextIntlPlugin from "next-intl/plugin";
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // TO Do
+  // domains to accept for backend stuff
+};
+
+export default withNextIntl(nextConfig);
