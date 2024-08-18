65e21214d33cf7f3976c3cdfae14485bf4dbd000
YasseRRRRRRRR <yassermoussaddak3@gmail.com>
2024-08-18 11:39:23 +0300

    images now load from the bucket

--- src/app/[locale]/[id]/page.tsx
+++ src/app/[locale]/[id]/page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import supabase from "../config/supabaseClient";
 
 const ProductPage = async ({
   params: { locale, id },
 }: {
   params: { locale: string; id: number };
 }) => {
-  return <div>Product {id}</div>;
+  let { data: Product, error } = await supabase
+    .from("Products")
+    .select("name")
+    .eq("id", `${id}`)
+    .limit(1)
+    .single();
+  return (
+    <div>
+      Product {id} is {Product?.name}
+    </div>
+  );
 };
 
 export default ProductPage;
--- src/app/[locale]/components/Products.tsx
+++ src/app/[locale]/components/Products.tsx
@@ -3,18 +3,18 @@ import Link from "next/link";
 import supabase from "../config/supabaseClient";
 import Image from "next/image";
 
-const fetchProducts = async () => {
-  const { data } = await supabase.from("Products").select("*");
-
-  return data;
-};
+// const fetchProducts = async () => {
+//   let { data } = await supabase.from("Products").select("*");
+//   return data;
+// };
 
 const Products = async ({
   params: { locale },
 }: {
   params: { locale: string };
 }) => {
-  const products = await fetchProducts();
+  let { data: Products, error } = await supabase.from("Products").select("*");
+
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -23,7 +23,7 @@ const Products = async ({
         </h2>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products?.map((product) => (
+          {Products?.map((product) => (
             <Link
               href={`/${locale}/${product.id}`}
               key={product.id}
@@ -34,14 +34,14 @@ const Products = async ({
                   width={280}
                   height={380}
                   alt="image"
-                  src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/product_images/${product.image}`}
+                  src={`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/product_images/${product.image}`}
                   className="h-full w-full object-cover object-center lg:h-full lg:w-full"
                 />
               </div>
               <div className="mt-4 flex justify-between">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <a href="#">
+                    <a href="">
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
                     </a>
@@ -49,7 +49,7 @@ const Products = async ({
                   <p className="mt-1 text-sm text-gray-500">green</p>
                 </div>
                 <p className="text-sm font-medium text-gray-900">
-                  {product.price}
+                  € {product.price}
                 </p>
               </div>
             </Link>
@@ -57,6 +57,7 @@ const Products = async ({
         </div>
       </div>
     </div>
+    // this would be ideally the main product page
   );
 };
 
