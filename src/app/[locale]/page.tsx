cd73c91f6483b4fd2aba838a0bd1dd5d51bb9ee7
YasseRRRRRRRR <yassermoussaddak3@gmail.com>
2024-08-28 18:28:45 +0300

    backend for how to feth stuff done, now for the adding to cart and stuff like that

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -14,9 +14,24 @@ export default async function Home({
   params: { locale: string };
 }) {
   const supabase = createClient();
+  const { data: user, error } = await supabase.auth.getUser();
 
-  let { data: orders } = await supabase.from("orders").select("*");
-  const { data, error } = await supabase.auth.getUser();
+  let { data: orders } = await supabase
+    .from("orders")
+    .select()
+    .eq("user_id", user.user?.id);
+
+  // this feels hacked toghe
+  let user_order = 0;
+  if (orders) {
+    user_order = orders[0].id;
+  }
+  let { data: order_items } = await supabase
+    .from("order_items")
+    .select()
+    .eq("order_id", user_order);
+
+  // .eq("", orders?[0].id);
   // if (error || !data?.user) {
   //   redirect(`/${locale}/login`);
   // }
@@ -30,16 +45,12 @@ export default async function Home({
         <>L90wada</>
       )}{" "}
       <br /> */}
-      {data.user ? (
-        <>logged in as {data.user?.email}</>
-      ) : (
-        <>user not logged in</>
-      )}
+      {user ? <>logged in as {user?.user?.email}</> : <>user not logged in</>}
       <br />
       {/* {isAdmin ? <>admin bravo</> : <>not admin</>} */}
       <Testing thing={orders} />
       {/* <code>{JSON.stringify(reviews, null, 2)}</code> */}
-      {/* <div>
+      <div>
         {orders?.map((order, i) =>
           order ? (
             <ul key={i} className="flex flex-col">
@@ -52,7 +63,20 @@ export default async function Home({
             <>policy is fucked</>
           )
         )}
-      </div> */}
+        {order_items?.map((order, i) =>
+          order ? (
+            <ul key={i} className="flex flex-col">
+              {" "}
+              <li>
+                product n0{i + 1}: {order.price}€
+              </li>
+            </ul>
+          ) : (
+            <>policy is fucked</>
+          )
+        )}
+      </div>
+
       {/* <LoginLink>Sign in</LoginLink> */}
       {/* <RegisterLink>Sign up</RegisterLink> */}
       <Link href={`${locale}/login`}>Login</Link>
